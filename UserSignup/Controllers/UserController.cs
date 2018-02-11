13fc788e67636efd7defdbb35621fa7bee5cc53e
MehUserAcc <36061263+MehUserAcc@users.noreply.github.com>
2018-02-11 11:52:18 -0700

    Add files via upload

---
+++ UserSignup/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserSignup.Models;
+using System.Text.RegularExpressions;
+
+namespace UserSignup.Controllers
+{
+    public class UserController : Controller
+    {
+        private static UserData userData;
+
+        static UserController()
+        {
+            userData = UserData.GetInstance();
+        }
+
+        public IActionResult Index(User user)
+        {
+            if (user == null) user = new User{
+                Username = "",
+                Password = "",
+                Email = "",
+                FavColor = "" };
+            //List<User> users = UserData.GetAll();
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {
+            ViewBag.Error = null;
+
+            if (user.Password != verify)
+            {
+                ViewBag.Errors += "Passwords do not match<br/>";
+            }
+
+            if(user.Username == null || user.Email == null)
+            {
+                ViewBag.Errors += "One or more fields are empty<br/>";
+            }
+
+            if (user.Username.Length < 5 || user.Username.Length > 15 
+                || !Regex.IsMatch(user.Username, @"^[a-zA-Z]+$"))
+            {
+                ViewBag.Errors += "Username is invalid<br/>";
+            }
+
+            if(ViewBag.Errors)
+            {
+                return View();
+            }
+            UserData.Add(user);
+            return View("Details", user.UserId);
+        }
+
+
+    }
+}
---
+++ UserSignup/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSignup.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FavColor { get; set; }
+        public int UserId { get; set; }
+        private static int nextId = 1;
+        public DateTime CreateDate;
+
+        public User ()
+        {
+            UserId = nextId;
+            nextId++;
+            CreateDate = DateTime.Now;
+        }
+
+        public User(string Username, string Email, string Password, string FavColor)
+        {
+            this.Username = Username;
+            this.Email = Email;
+            this.Password = Password;
+            this.FavColor = FavColor;
+            UserId = nextId;
+            nextId++;
+            CreateDate = DateTime.Now;
+        }
+    }
+}
